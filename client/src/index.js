a516c1b6df95b9635adccb6cabb74bea69b64d1c
sebekgr <sebekgr@o2.pl>
2018-02-13 09:52:28 +0000

    redux store logger fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
-import recuders from './reducers';
+import { createStore, applyMiddleware, compose} from 'redux';
+import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
 
-if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-	const store = createStore(recuders,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(reduxThunk));
-} else {
-	const store = createStore(recuders, applyMiddleware(reduxThunk));
-}
+process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+	applyMiddleware(reduxThunk),
+
+  );
+
+  const store = createStore(reducers, enhancer);
+
 
 
 
